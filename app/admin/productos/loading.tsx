9c42c5febec1af6463cfe50e49628723ec26c003
Iara <iaravera3333@gmail.com>
2024-06-23 08:00:29 -0300

    cambios minimos

---
+++ app/admin/productos/loading.tsx
@@ -0,0 +1,5 @@
+import { ProductGridSkeleton } from '@/app/ui/skeletons';
+
+export default function Loading() {
+    return <ProductGridSkeleton />
+  }
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -2,8 +2,8 @@ import { inter, lusitana } from '@/app/ui/fonts';
 
 export default function Header() {
   return (
-    <div className="flex y-0 p-6 mb-6 md:overflow-y-auto w-full bg-gray-200 justify-center">
-      <h2 className={`${lusitana.className} text-green-900 text-3xl font-bold tracking-tight text-center text-white`}>
+    <div className="flex y-0 p-6 mb-6 md:overflow-y-auto w-full bg-gradient-to-r from-gray-800 to-black justify-center">
+      <h2 className={`${lusitana.className} text-green-900 text-3xl font-bold tracking-tight text-center text-white `}>
         Hola Vegan
       </h2>
     </div>
