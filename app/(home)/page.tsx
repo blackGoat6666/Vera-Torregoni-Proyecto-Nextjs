433372bc8d36a5c517a139319c8b1c0d28e3a9cd
Iara <iaravera3333@gmail.com>
2024-06-23 23:19:21 -0300

    Cambio en home

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,12 @@
 import { Recetas } from "../ui/home/recetas";
+import Welcome from "../ui/home/welcome";
 
 export default function Page() {
     return (
-    <div className="flex flex-row">
+    <div className="flex flex-col">
+      <Welcome/>
+      <br></br>
+      <h1 className="text-2xl font-black text-gray-600 p-10">Recetas para inspirarte</h1>
       <Recetas></Recetas>
     </div>
     );
