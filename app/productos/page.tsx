4143fca287d0289c7b2336eea9228c16c3f9a0e7
Iara <iaravera3333@gmail.com>
2024-06-23 22:58:35 -0300

    Paginacion

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -3,6 +3,7 @@ import Header from '@/app/ui/header';
 import ProductsGrid from '../ui/products/products-grid';
 import Search from '../ui/search';
 import { fetchProductsPages } from '../lib/data';
+import Pagination from '../ui/pagination';
 
 
 
@@ -21,6 +22,9 @@ export default async function Page({
     <div className='flex flex-col w-full'>
           <div className='w-2/3 p-4 px-10'> <Search placeholder="Search invoices..." /></div>
           <ProductsGrid  query={query} currentPage={currentPage} />
+          <div className="mt-5 flex w-full justify-center p-10">
+            <Pagination totalPages={totalPages} />
+          </div>
     </div>
   );
 }
