f3dc90de7fa9033c4d01eb6e963610a9ac3a7bbe
Iara <iaravera3333@gmail.com>
2024-07-01 22:06:08 -0300

    busqueda y paginacion productos grilla admin

--- app/admin/productos/page.tsx
+++ app/admin/productos/page.tsx
@@ -1,11 +1,28 @@
+import { fetchProductsPages } from '@/app/lib/data';
 import AdminProductsGrid from '@/app/ui/admin/admin-products-grid';
+import Pagination from '@/app/ui/pagination';
+import Search from '@/app/ui/search';
 
 
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}){
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchProductsPages(query);
   return (
-    <div className='flex w-full'>
-        <AdminProductsGrid />
+    <div className='flex flex-col w-full'>
+          <div className='w-2/3 p-4 px-10'> <Search placeholder="Search invoices..." /></div>
+          <AdminProductsGrid  query={query} currentPage={currentPage} />
+          <div className="mt-5 flex w-full justify-center p-10">
+            <Pagination totalPages={totalPages} />
+          </div>
     </div>
   );
 }
